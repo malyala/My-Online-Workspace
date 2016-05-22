9188bbf6df007cdb84d70b822e26e6848f46f32f
Divesh Otwani <divesh.otwani@gmail.com>
2016-05-21 21:15:47 -0400

    made mega-tic-tac-toe

---
+++ Java/Learning/Message.java
@@ -0,0 +1,9 @@
+//import javax.swing.JOptionPane;
+
+public class Message{
+    public static void main(String[] args) {
+       javax.swing.JOptionPane.showMessageDialog(null, "Welcome to Java!");
+    }
+} 
+
+
--- Java/Learning/Welcome.java
+++ Java/Learning/Welcome.java
@@ -1,6 +1,13 @@
+public class Welcome {
+    public static void main(String[] args){
+        System.out.println("Welcome to Java!");
+    }
+}
 
+//this is a line comment
 
-
+/* this is a 
+multi line comment */
 
 
 
