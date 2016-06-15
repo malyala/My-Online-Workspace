d1887fbb77d73beda9181ef1caa5038e5f27966b
Divesh Otwani <divesh.otwani@gmail.com>
2016-06-15 09:28:38 -0400

    Took out other parts
    took out the unimportant parts

--- Python/Projects for or related to some class/Computer Science Courses/Introduction H107/Selected Work From Introductory Course/graph_coloring problem.py
+++ Python/Projects for or related to some class/Computer Science Courses/Introduction H107/Selected Work From Introductory Course/graph_coloring problem.py
@@ -200,9 +200,6 @@ Ar Br Cb
 
 
 
-
-
-
 # make Python look in the right place for logic.py
 try:
     import sys
@@ -218,187 +215,7 @@ except:
 from is_legal import is_a_legal_coloring, are_we_doing_doctest, we_are_doing_doctest, we_are_doing_coloring_enumeration
 
 
-
-
-
-
-
-
-
-
-
-
-"""
-============================================================
-                ======================
-                collect_all_colorings design comment and test suite:
-                ========================
-==================================================================
-#############Part I: Comment##########################
-    
-    Introduction:    
-    
-        This uses complex recursive design as discussed in 4.6.2
-
-        Essentially the answer is broken down into three sections:
-        1. Ar + ..
-        2. Ag + ..
-        3. Ab + ..
-
-        the dots are collect all colorings for all except the first state.
-        This concatenation of one string to every other string is a simpler but different problem
-        we combine these three parts in each recursive call to give the whole answer
-
-    Five steps enumerated below:
-
-
-
-    1. Within a collect all colorings, there is another collect all colorings of everything but the first state
-
-    2. If you save the list of the smaller recursive call
-    and make three other lists where you add the missed first element with one of the three colors
-    and then combine the three lists (or strings, rather), you get the solution to the whole problem
-
-    we could call this a smaller slightly different problem called triple
-
-        ===========================
-
-        #Comment and test suite of triple:
-        
-            triple would take in the one omitted state, the string of colorings w/o the omitted state
-            and the string of the allowed colors
-    
-            It would then use basic recursion to return a string of the omitted state
-            with the first color in the color string added to the string of colorings without the
-            omitted state added to the recursive call of triple that excludes the first color in the coloring string 
-            and keeps the other params the same.
-        
-            see:
-                triple('A', 'Br\nBg', 'rg')
-                returns 'Ar' + 'Br' + '\n' + "Ar' + 'Bg' + ... + triple(same, same, 'g')
-                the recusive call would just be the base case here 
-                -the base case would have to be the empty coloring string
-    
-            A basic test suite:
-    
-            >>> triple('A', "Br\nBg", "rg")
-            Ar Br
-            Ar Bg
-            Ag Br
-            Ag Bg
-    
-            >>>triple("A", "Ba", "a")
-            Aa Ba
-    
-        note- I'm not sure about how this will be represented but in this case that does not seriously 
-        change the algorithm. 
-
-        =============================
-
-    3. If we have one state X, we can simply return  Xr Xg Xb
-
-    4. see number 3
-
-    5. If the number of states is greater than or equal to 1 and we keep ommiting the first, we must reach 
-    a call of just one state
-
-
-
-
-
-
-    Test suite for collect all colorings:
-    this is implemented as a function called generate (or generate_no_loops) which is called 
-    inside collect all_legal_colorings
-    [abreviated cac()]
-
-        >>> cac("A", "rgb")
-        Ar
-        Ag
-        Ab
-
-        >>> cac("AB", "rgb")
-        Ar Br
-        Ar Bg
-        Ar Bb
-        Ag Br
-        Ag Bg
-        Ag Bb
-        Ab Br
-        Ab Bg 
-        Ab Bb
-
-        >>>cac("ABC", "rgb")
-        'Ar Br Cr'
-        'Ag Br Cr'
-        'Ab Br Cr'
-        'Ar Bg Cr'
-        'Ag Bg Cr'
-        'Ab Bg Cr'
-        'Ar Bb Cr'
-        'Ag Bb Cr'
-        'Ab Bb Cr'
-        'Ar Br Cg'
-        'Ag Br Cg'
-        'Ab Br Cg'
-        'Ar Bg Cg'
-        'Ag Bg Cg'
-        'Ab Bg Cg'
-        'Ar Bb Cg'
-        'Ag Bb Cg'
-        'Ab Bb Cg'
-        'Ar Br Cb'
-        'Ag Br Cb'
-        'Ab Br Cb'
-        'Ar Bg Cb'
-        'Ag Bg Cb'
-        'Ab Bg Cb'
-        'Ar Bb Cb'
-        'Ag Bb Cb'
-        'Ab Bb Cb'
-
-==================================================================
-                ======================
-   End of collect_all_colorings design comment and test suite
-                ========================
-==================================================================
-
-"""
-
-
-        #PART I: Generate
-#########################################################################################################
-
-
-    #Code to be graded: (However, I have seperate code that I would submit for code review-see below)
-#======================Doing it without loops relying on 3 colors==============================
-def generate_no_loops2(states, colors):
-    return prefix_function2("", states, colors) + "\n" #this is for consistency         
-                                                   
-def prefix_function2(prefix, states, colors):
-    precondition( all([type(prefix)==str, type(states)==str, type(colors)==str]) and len(states) >= 1 and len(colors) >= 1)
-    if len(states) ==1:
-        prefix += states
-        ret = prefix + colors[0] + "\n" + prefix + colors[1] + "\n" + prefix + colors[2]
-        #postcondition is that for 3 colors you could print out 3 lines with the prefix added to the state added to a 
-        #different color in the colors string on each line
-        return ret
-    else:
-        ret = prefix_function2(prefix + states[0] + colors[0] + " ", states[1::], colors) + \
-        "\n" + prefix_function2(prefix + states[0] + colors[1] + " ", states[1::], colors) + \
-        "\n" +  prefix_function2(prefix + states[0] + colors[2] + " ", states[1::], colors)        
-        #postcondition is that a list of each line if printed contains every posible coloring
-        return ret
-#==============================Finished doing it without loops relying on three colors=====================  
-
-
-#=======I would prefer this code to be used for code review should we have one===========
-#=======However, if the previous one is preferred, a peer reviewer can do that instead===
-
-#====================My sneaky recursion method for n colors:============================
-
-     #I have left out most comments as a challenge(should you choose to accept it, see above)
-        
+######################### PART I: Generate #########################################        
 def generate_no_loops(states, colors):
     return prefix_function("", states, colors)          
                                                                                                                                       
@@ -413,13 +230,6 @@ def prefix_per_color(ret, remaining_colors, states, colors, prefix):
         if ret[-1]!='\n': ret += '\n' 
         return prefix_per_color(ret, remaining_colors[1::], states, colors, prefix)  
         
-#================================== End of Sneaky recursion =============================
-#========================================================================================
-
-
-
-
-
 ####################################################################################################            
 #End of part I: generate
 ####################################################################################################
@@ -433,24 +243,20 @@ def filter_wrapper(all_colorings, states, borders):
     #precondition: just have all the states be included in all colorings with the correct colors
     #the borders have no extra states or extra colors and so on
     #the borders should also not be empty
-    
     cut_points = 3*len(states)
     #postcondition: only colorings that have no states which share a border havving the same color
     #are returned in a string seperated by "\n"s
     return real_filter(all_colorings, cut_points, borders)
 
 def real_filter(all_colorings, cut_points, borders):
-    
     if len(all_colorings) == cut_points:
         return helper(all_colorings, borders)
     else:
         return helper(all_colorings[:cut_points], borders) + real_filter(all_colorings[cut_points:], cut_points, borders)
 
 def helper(one_coloring, borders):
-    if is_a_legal_coloring(one_coloring[:-1], borders):  #we cut off the '\n'
-        return one_coloring #this inculdes the '\n'
-    else:
-        return ''
+    if is_a_legal_coloring(one_coloring[:-1], borders): return one_coloring #this inculdes the '\n'
+    else: return ''
                 
 ##################################################################################################
 #End of part II: Filter
@@ -584,28 +390,4 @@ if __name__ == "__main__":
         print "Congratulations! You have passed all" , result[1], "coloring enumeration tests"
     else:
         print "Rats!"
-#This follwoing function varies the coloring of the
-#"leading" state and returns all possible colorings with that change
-#and a prefix before it
-#Thus it creates a prefix_function call per each coloring with the 
-#remaining_colors of the first state
-    #Ex:
-#>>> print prefix_per_color('', 'b', 'AB', 'rgb', 'Test_prefix_')
-#Test_prefix_Ab Br
-#Test_prefix_Ab Bg
-#Test_prefix_Ab Bb
-    #Notice that the "leading" state "A" is paired with 
-    #the remaining color 'b' and that new prefix "Test_prefix_Ab "
-    #(notice the space) is added to each coloring of state"B" with 
-    #colors "rgb"
-#
 
-#This gives you a prefix (just a portion of one coloring like "Cr " -notice the space)
-#added to all colorings of given states and colors
-  #Ex:
-#>>> print prefix_function("Test_prefix_", 'AB', 'rg')
-#Test_prefix_Ar Br
-#Test_prefix_Ar Bg
-#Test_prefix_Ag Br
-#Test_prefix_Ag Bg
-#    
