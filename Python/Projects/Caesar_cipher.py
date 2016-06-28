d5e9cc3ccb95b033cc3354ec2b27b5035272d9dd
Divesh Otwani <divesh.otwani@gmail.com>
2016-06-27 23:27:54 -0400

    little project

---
+++ Python/Projects/Caesar_cipher.py
@@ -0,0 +1,31 @@
+"""
+A caesar cipher shifts the alphabet right a few letters and replaces each letter in a message to encode it.
+
+If we shift the alphabet right 1 letter, we have:
+
+>>> plaintext = "test"
+>>> cipher = encrypt(plaintext, 1)
+>>> cipher
+uftu
+>>> decrypt(cipher, 1)
+test
+
+"""
+
+def encrypt(word, shift_right):
+    import string
+    ret = ''
+    shift_dict = alphabet_dict(string.ascii_lowercase, shift_right)
+    shift_dict = alphabet_dict(string.ascii_uppercase, shift_right, shift_dict)
+    for letter in word:
+        ret += shift_dict[letter]
+    return ret
+
+def alphabet_dict(alphabet, shift_right,ret = dict()):
+    shifted = alphabet[shift_right:] + alphabet[:shift_right]
+    for i in range(len(alphabet)):
+        ret[alphabet[i]] = shifted[i]
+    return ret
+
+def decrypt(cipher, shift_right):
+    return encrypt(cipher, 26-shift_right)
