b261b7d75ad6636a5e5e063316ddfdea6147e6d9
Divesh Otwani <divesh.otwani@gmail.com>
2016-07-10 17:03:55 +0000

    finish a bit of pointers

--- C/Expirementation/pointers2.c
+++ C/Expirementation/pointers2.c
@@ -1,10 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//general usage of pointers:
+
+/*
+1. declare the pointer (this just assigns some free poiter-sized bit of memory to x)
+2. assign memory to the pointer (otherwise, it points to some unaccessible point of memory,)
 
 
-int main(){
+
+*/
+
+int main()
+{
     int *x;
-    
+    x = malloc(sizeof(int));
     *x = 7;
-    printf("%d\n", *x);
+    printf("*x is %d\n", *x);
+    // the pointer x, has an adress of itself
+    printf("&x is %p\n", &x);
+    // the pointer's value is the adress of the place 7 is stored
+    printf("x itself is %p\n", x); //%p is print type pointer
     
     
     return 0;
