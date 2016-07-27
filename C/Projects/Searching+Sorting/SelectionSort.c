e0c4f9c8b66a1ec2850753524245834d71873b2c
Divesh Otwani <divesh.otwani@gmail.com>
2016-07-27 11:03:36 -0400

    Commit at time Wed Jul 27 11:03:36 EDT 2016

---
+++ C/Projects/Searching+Sorting/SelectionSort.c
@@ -0,0 +1,20 @@
+/*
+In this file, I implement a generic selection sort
+which takes in
+	1) An array
+	2) The size of the array (number of elements)
+	3) The size of each element of the array
+	4) A function that takes in two pointers to elements of the array, p1, p2
+		and returns 1 iff (*p1) <= (*p2)
+		and -1 otherwise
+	and sorts the array in ascending order
+
+*/
+
+
+
+
+
+
+
+
