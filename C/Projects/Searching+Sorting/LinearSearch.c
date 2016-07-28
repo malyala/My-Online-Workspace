207848acb285b7d4844d027c5d7121222ea2a45f
Divesh Otwani <divesh.otwani@gmail.com>
2016-07-28 09:59:06 -0400

    Commit at time Thu Jul 28 09:59:06 EDT 2016

--- C/Projects/Searching+Sorting/LinearSearch.c
+++ C/Projects/Searching+Sorting/LinearSearch.c
@@ -2,23 +2,33 @@
 #include <stdlib.h>
 
 enum boolean {False, True};
+typedef int (*MatchCriterion)(void *item);
 
-boolean LinearSearch(int * array, int len, int item)
+boolean LinearSearch(void *array, int len, size_t size, MatchCriterion fn)
 {
 	for (int i=0; i < len; ++i)
 	{
-		if (array[i]==item)
+		if (fn(((char *) array) + (i * size)))
 			return True;
 	}
 	return False;
 }
 
+int Is4(void *);
+int Is0(void *);
 int main()
 {
 	int *arr = (int *) malloc(4 * sizeof(int));
 	arr[0] = 1; arr[1] = 2; arr[2] = 3; arr[3] = 4;
-	printf("Search test 1: %d\n", LinearSearch(arr, 4,4));
-	printf("Search test 0: %d\n", LinearSearch(arr, 4,0));
+	printf("Search test 1: %d\n", LinearSearch(arr, 4,sizeof(int), Is4));
+	printf("Search test 0: %d\n", LinearSearch(arr, 4,sizeof(int), Is0));
 	free(arr);
 	return 0;
+}
+
+int Is4(void *x){
+	return *((int *) x)==4;
+}
+int Is0(void *x){
+	return *((int *) x)==0;
 }
