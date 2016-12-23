c531d8a8607adc1db1d6fb90257bf43d7214d6c1
Divesh Otwani <divesh.otwani@gmail.com>
2016-12-23 14:55:21 -0500

    Commit at time Fri Dec 23 14:55:21 EST 2016

--- C/Data_Structures/LinkedList/linkedList.c
+++ C/Data_Structures/LinkedList/linkedList.c
@@ -43,6 +43,7 @@ void *llIndex(LL *list, int i){
 
 int llInsert(LL *list, void *val, int index){
 	//We insert before the element at index index
+	//Append by inserting at index == list->len
 	assert((index > -1) && (index <= (list->len)));
 		//1. Make the node to insert
 	Node *ret = (Node *) malloc(sizeof(Node));
@@ -54,8 +55,6 @@ int llInsert(LL *list, void *val, int index){
 	}
 		//2. Insert that node properly
 	if(list->len != 0){
-		//Insert before the node at index index.
-		//Append by inserting at index == list->len
 		if(index==0){
 			ret->next = list->head;
 			list->head = ret;
@@ -93,9 +92,7 @@ void *llPop(LL *list, int index){
 		list->head = list->head->next; // This could be NULL
 		ret = hold->val;
 		free(hold);
-		if(list->len == 1){
-			list->tail = NULL;
-		}
+		list->tail = list->len==1 ? NULL: list->tail;
 	}else{ 
 		//a non-empty list with index>0
 		//has 2 or more elems
@@ -104,15 +101,76 @@ void *llPop(LL *list, int index){
 		ret = toFree->val;
 		hold->next = toFree->next; //Could be NULL
 		free(toFree);
-		if (list->len==2){
-			list->tail = list->head;
-		}else if (index == (list->len)-1){
-			list->tail = hold;
-		}
+		list->tail = list->len==2 ? list->head : list->tail;
+		list->tail = index ==(list->len -1)? hold : list->tail;
 	}
 	list->len -= 1;
 	return ret;
 }
+// ---------------------------------------------------------------------------
+//Exercises from http://cslibrary.stanford.edu/105/LinkedListProblems.pdf
+
+
+//Problem 6
+typedef int (*comparer)(void *, void *);
+	// comp(&a, &b) is 1 iff a < b and 0 otherwise
+void SortedInsert(LL *list, void *val, comparer comp, int length){
+	int len = length == -1 ? llLen(list): length;
+	for(int i=0; i<= len; ++i){
+		if(i==len){
+			llInsert(list, val, i);
+		}else if(comp(val, llIndex(list, i))){
+			llInsert(list, val, i);
+			break;
+		}
+	}
+}
+
+//Problem 7
+void InsertSort(LL *list, comparer comp){
+	int len = llLen(list);
+	if(len!=1){
+		for(int i=1; i < len; ++i){
+			SortedInsert(list, llPop(list, i), comp, i);
+		}
+	}
+}
+
+//Problem 8
+void AppendLL(LL *list1, LL *list2){
+	//Appends list2 to list1
+	//and nulls out list2
+	list1->tail->next = list2->head;
+	list1->len += list2->len;
+	list1->tail = list2->tail;
+	free(list2); //We don't free the nodes, those
+				// are needed
+}
+
+
+
+//Problem 10
+				//comp(&a, &b) is 1 iff a==b and 0 otherwise
+void print(LL *);
+void RemoveDuplicates(LL *list, comparer comp){
+	//Precond: the list is sorted and non-empty.
+	if(llLen(list) > 1){
+		Node *before = list->head;
+		Node *curr = before->next;
+		int index = 1;
+		while(curr != NULL){
+			if(comp(curr->val, before->val)){
+				llPop(list, index);
+				curr = before->next;
+			}else{
+				index++;
+				before = curr;
+				curr = curr->next;
+			}
+		}
+	}
+}
+
 
 
 
@@ -127,32 +185,76 @@ void print(LL *list){
 		start = start->next;
 	}
 	printf("}\n");
+	printf("head: @%p %d\n", list->head, *((int *) list->head->val));
+	printf("tail: @%p %d\n", list->tail, *((int *) list->tail->val));
+	printf("len: %d\n", list->len);
 }
 
+
+LL *IntLLMaker(int size, void *arr){
+	int *array = (int *) arr;
+	LL *ret = CreateLL();
+	for(int i=0; i < size; ++i){
+		llAppend(ret, array + i);
+	}
+	return ret;
+}
+
+int intComp(void *a, void *b){
+	return *((int *)a) < *((int *) b);
+}
+
+
+int intequal(void *a, void *b){
+	return *((int *) a) == *((int *) b);
+}
+
+
+
 int main(){
-	int x[4] = {1,2,3,4};
+	
+
+
+	int x[5] = {1,2,3,4,5};
+	LL *test = IntLLMaker(5, x);
+	print(test);
+	int y = 3, z = -2, e = 7;
+	SortedInsert(test, &y, intComp, -1);
+	print(test);
+	SortedInsert(test, &z, intComp, -1);
+	print(test);
+	SortedInsert(test, &e, intComp, -1);
+	print(test);
+	// Testing problem 7
+	printf("\nTesting problem 7\n\n");
+	int a[11] = {-3, 45, 12, 10, 9, 8, 33, 33, 46, -4, 13};
+	LL *test2 =IntLLMaker(11, a);
+	puts("Before sort: ");
+	print(test2);
+	InsertSort(test2, intComp);
+	puts("After sort: ");
+	print(test2);
+	// Testing problem 8
+	printf("\nTesting problem 8\n");
+	print(test);
+	print(test2);
+	AppendLL(test, test2);
+	print(test);
+	// Testing problem 9
+	printf("\nTesting problem 9\n");
+	InsertSort(test, intComp);
+	print(test);
+	RemoveDuplicates(test, intequal);
+	print(test);
+
+	
+	/*
 	int y[7] = {1,2,3,4,5,6,7};
-	LL *test = CreateLL();
 	LL *test2 = CreateLL();
 	print(test2);
-	//for(int i = 0; i<4; ++i){
-	//	llAppend(test, x + i);
-	//}
 	for(int j=0; j<7; ++j){
 		llAppend(test2, y+j);
-		//print(test2);
-		//printf("tail %p\n",test2->tail);
-		//printf("tail val %d\n", *((int *) test2->tail)  );
 	}
-
-	//for(int i =0; i<4; ++i){
-	//	printf("%d ", *((int *) llIndex(test, i )));
-	//}
-	//putchar('\n');
-	//for(int i =0; i<4; ++i){
-	//	printf("%d ", *((int *) llPop(test, llLen(test) - 1)));
-	//}
-
 	putchar('\n');
 	printf("Misc testing of insert and pop.\nI trust index and len are fine.\n");
 	printf("Original: ");
@@ -181,6 +283,7 @@ int main(){
 	print(test2);
 	printf("tail val %d\n", *((int *) test2->tail->val)  );
 	return 0;
+	*/
 }
 
 
