24a9e74bcc85dc70712c022df5dacc6e79c9ceee
Divesh Otwani <divesh.otwani@gmail.com>
2017-01-14 23:12:25 -0500

    Commit at time Sat Jan 14 23:12:25 EST 2017

---
+++ C++/Expirementation/newArrays.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+
+int main(){
+	int *x[3] = new int[2][3];
+
+	return 0;
+}
+
+
+
+
+
+
---
+++ Python/Projects/spyfall.py
@@ -0,0 +1,28 @@
+"""
+This implements the poorly coded spyfall game on http://spyfall.adrianocola.com/
+
+Run game(number of players)
+
+
+"""
+
+
+class location:
+    def __init__(self, name, roles):
+        self.name = name;
+        assert(type(name)==str);
+        self.roles = roles;
+        assert(type(roles)==list);
+
+
+
+class Game:
+    def __init__()
+
+
+
+
+
+
+
+
